14a2940ccbd7c09299d8026bcd3991c9d8cc53fd
Arsalan Khan <113425371+IamArsalanKhan@users.noreply.github.com>
2023-08-29 11:14:06 +0500

    Add files via upload

---
+++ Task 02/README.md
@@ -0,0 +1,2 @@
+# Task-02
+ Task 02 of Assignment 45 questions of typescript
--- Task 02/task02.ts
+++ Task 02/task02.ts
@@ -1,1 +1,6 @@
+// Personal Message: Store a person’s name in a variable, and print a message to that
+// person. Your message should be simple, such as, “Hello Eric, would you like to learn
+// some Python today?”
 
+let personalMessage = "Arsalan Khan";
+console.log("\"Hello " + personalMessage + ", would you like to learn some python today?\"");
